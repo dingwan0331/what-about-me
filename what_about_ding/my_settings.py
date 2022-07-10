dc969a924d67a09b6c7cd913b4345707965fc27f
dingwan <dingwan0331@gmail.com>
2022-07-10 21:45:03 +0900

    project initial-settings

---
+++ what_about_ding/my_settings.py
@@ -0,0 +1,14 @@
+DATABASES = {
+    'default' : {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'what_about_ding', 
+        'USER': 'root',
+        'PASSWORD': '45jung45!!',
+        'HOST': '127.0.0.1',
+        'PORT': '3306', 
+    }
+}
+
+SECRET_KEY = 'django-insecure-r$2xos8=akth%zg4qdj=$3**y8iw(=_rf=t#l!$p1m2--9)j-9'
+
+ALGORITHM = 'HS256'
